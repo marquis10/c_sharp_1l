e4694be0f2c96bc173fe845f6ee9b467e54eff34
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-11-27 23:17:36 +0500

    Имена

---
+++ 5proga/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Саша");
+{
+    Console.WriteLine("Ура, это же Саша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
