f480e17de985470115ce3907d48ea0ae323584b9
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-01 17:22:48 +0500

    tochki2

--- tochki/Program.cs
+++ tochki/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();		// очистка экрана
+int xa = 40, ya = 1,		// определяем вершины треугольника
+     xb = 1, yb = 30,
+     xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);	// Постановка курсора в нужную позицию
+Console.WriteLine("+"); // Вывод выбранной позиции
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;	// Определение случайной точки x
+
+int count = 0;
+
+
+while(count < 10000)
+{
+	int what = new Random().Next(0, 3);	// Сгенерируем случайное число [0;3) в интервале от 3, ограничение 3. т.е. выбор между числами 0,1,2
+	if(what == 0) // Проверка
+	{
+	x = (x + xa)/2;		// Присвоим x середину отрезка
+	y = (y + ya)/2;
+}
+if(what == 1) // Проверка
+	{
+	x = (x + xb)/2;		
+	y = (y + yb)/2;
+}
+if(what == 2) // Проверка
+	{
+	x = (x + xc)/2;		
+	y = (y + yc)/2;
+}
+Console.SetCursorPosition(x, y);
+Console.WriteLine("+");
+count++;	// или +=1
+}
